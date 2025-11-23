9e3a189f8a829473051b11e43e965a0ba623d792
Petterson Ferraz <shinokzn1227@gmail.com>
2025-11-22 23:00:36 -0300

    adicionadas classes de security

---
+++ src/main/java/com/devPetter/usuario/infrastructure/repository/EnderecoRepository.java
@@ -0,0 +1,9 @@
+package com.devPetter.usuario.infrastructure.repository;
+
+import com.devPetter.usuario.infrastructure.entity.Endereco;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EnderecoRepository extends JpaRepository<Endereco, Long> {
+}
